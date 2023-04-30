1baa6f93728d272ed31fcf4c27a850bd48f2b566
LaloSL <127783873+LaloSL@users.noreply.github.com>
2023-04-29 22:03:11 -0300

    Update PruebaArreglo.java

--- Tpoarreglos/src/PruebaArreglo.java
+++ Tpoarreglos/src/PruebaArreglo.java
@@ -1,6 +1,7 @@
 public class PruebaArreglo {
     public static void main(String[] args) {
         int[] arregloDeEnteros = {1, 2, 3, 4, 5, 6, 7, 8, 10};
+        Arreglo.sumarLista(arregloDeEnteros);
         
 
 
