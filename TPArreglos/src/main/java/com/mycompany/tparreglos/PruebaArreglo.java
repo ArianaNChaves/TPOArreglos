af633ca4a5d1f6ee6fbf0fb9afbd66105606ff68
ArianaNChaves <navi.ariana99@gmail.com>
2023-05-01 17:47:06 -0300

    Pruebas terminadas

--- TPArreglos/src/main/java/com/mycompany/tparreglos/PruebaArreglo.java
+++ TPArreglos/src/main/java/com/mycompany/tparreglos/PruebaArreglo.java
@@ -3,11 +3,17 @@ package com.mycompany.tparreglos;
 public class PruebaArreglo {
     public static void main(String[] args) {
         int[] arregloDeEnteros = {1, 2, 3, 4, 5, 6, 7, 8, 10};
-        
-        //hola
-            
-        //hpaoosdsa
-        
-        
+         
+        int[][] arregloDeEnteros2 = {{12, 31}, {1, 3, 5}};
+        String vocales = "Texto de ejemplo";
+
+        Arreglo.sumarLista(arregloDeEnteros);
+
+        System.out.println("El mayor numero del arreglo es " + Arreglo.buscarMayor(arregloDeEnteros2));
+
+        System.out.println("El string " + vocales + " tiene " + Arreglo.cuentaVocales(vocales) + " vocales");
+
+        System.out.println("El string " + vocales + " tiene " + Arreglo.cuentaCaracter(vocales, 'r') + " veces el caracter r");
     }
+    
 }
